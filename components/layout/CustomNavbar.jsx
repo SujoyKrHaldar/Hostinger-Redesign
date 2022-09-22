25679e28e954f09aa9d8d9bb7637ecef6832b9c8
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-09-22 20:06:48 +0530

    login page - responsive

--- components/layout/CustomNavbar.jsx
+++ components/layout/CustomNavbar.jsx
@@ -8,12 +8,18 @@ function CustomNavbar() {
       <header className="absolute inset-0 py-9 w-full h-fit z-10">
         <div className="container flex items-center justify-between gap-4">
           <Link href="/">
-            <a className="inline-block w-[150px] h-[30px]">
-              <Img src="/logo_white.webp" alt="Logo" />
+            <a className="inline-block w-[150px]">
+              <Img
+                src="/logo_blue.png"
+                alt="Logo"
+                layout="responsive"
+                w={150}
+                h={30}
+              />
             </a>
           </Link>
 
-          <div className="cursor-pointer text-3xl hover:text-violet-700 duration-200">
+          <div className="cursor-pointer text-3xl p-1 bg-white rounded-md text-black duration-200">
             <HiMenuAlt3 />
           </div>
         </div>
--- components/login/Login.jsx
+++ components/login/Login.jsx
@@ -5,48 +5,36 @@ import Link from "next/link";
 function Login() {
   return (
     <section className="py-16 w-full h-screen">
-      <div className="absolute inset-0 w-1/2 h-full bg-black">
+      <div className="hidden xl:block  absolute top-0 right-0 w-full max-w-[700px] h-full">
         <Img
           src="https://images.unsplash.com/photo-1566228015668-4c45dbc4e2f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
           alt="Login Image"
-          className="opacity-50"
         />
+        <h2 className="text-white font-bold w-full h-fit max-w-md absolute 
+        left-1/2 bottom-28 -translate-x-1/2">
+          Go online with Hostinger and get started now. Make your perfect
+          website today.
+        </h2>
       </div>
-      <div className="absolute top-0 right-0 w-[350px] h-[350px] -z-10">
-        <div className="absolute w-full h-full inset-0 bg-gradient-to-t from-white z-10"></div>
-        <div className="absolute w-full h-full inset-0 bg-gradient-to-r from-white z-10"></div>
-        <Img src="/assets/pattern-2.svg" alt="pattern" />
-      </div>
-      <div className="container w-full h-full flex justify-between gap-4">
-        <div className="flex-1 h-full flex items-end">
-          <div className="space-y-3 py-16 max-w-md text-white">
-            <h1>
-              Login to{" "}
-              <span className="font-bold block text-7xl">Hostinger</span>
-            </h1>
-            <p className=" max-w-xs">
-              Go online with Hostinger and get started now. Make your perfect
-              website today.
-            </p>
-          </div>
-        </div>
 
-        <div className="flex-1 h-full flex items-center">
-          <div className="space-y-5 ml-auto max-w-sm">
-            <p className="text-3xl font-semibold">Login</p>
+      <div className="container h-full flex items-center justify-center xl:justify-start">
+        <div className="max-w-sm space-y-3 mt-4">
+          <h2 className="font-bold">
+            Login
+          </h2>
 
-            <LoginForm className="w-full" />
+          <LoginForm className="w-full" />
 
-            <p className="font-bold">
-              Not a member yet?{" "}
-              <Link href="/">
-                <a className="text-violet-700">Choose a hosting plan </a>
-              </Link>
-              and get started now!
-            </p>
-          </div>
+          <p className="font-bold">
+            Not a member yet?{" "}
+            <Link href="/">
+              <a className="text-violet-700">Choose a hosting plan </a>
+            </Link>
+            and get started now!
+          </p>
         </div>
       </div>
+      
     </section>
   );
 }
